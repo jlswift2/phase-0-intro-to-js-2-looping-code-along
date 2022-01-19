069075544ecd51c2442736ad0bbc5c821f4b1f4c
Justin Swift <jlswift2@gmail.com>
2022-01-18 18:38:00 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = []
+const event = 'birthday'
+
+function writeCards(names, event) {
+    const cards = []
+    for(let i = 0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return(cards)
+}
+
+function countDown(number) {
+    let countdown = number
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}

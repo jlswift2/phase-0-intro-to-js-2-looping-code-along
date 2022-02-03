6f4ab3a9fdeb59bdefd9a1d1bc602ebbf3540681
Justin Swift <jlswift2@gmail.com>
2022-02-02 18:15:46 -0600

    commit tree c15cd4cd876fda6c771635e6fe531a40d80f0e6a

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const names = []
-const event = 'birthday'
 
 function writeCards(names, event) {
-    const cards = []
-    for(let i = 0; i < names.length; i++){
-        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    const newArray= []
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     }
-    return(cards)
+    return newArray 
 }
 
-function countDown(number) {
-    let countdown = number
-    while (countdown >= 0) {
-        console.log(countdown--);
+
+
+
+function countDown(int) {
+    let counter = int
+    while(counter >= 0){
+    console.log(counter)
+    counter--
     }
 }
+
